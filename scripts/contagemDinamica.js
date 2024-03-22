c4b9e3a7a8c15e37216e6c8a8fea6bef5841028a
CarlosCampos <carloscampos.bn@gmail.com>
2024-03-21 21:09:41 -0300

    New Update

---
+++ scripts/contagemDinamica.js
@@ -0,0 +1,19 @@
+let valueDisplay=document.querySelectorAll(".lembreteNumber");
+let interval=4000;
+
+valueDisplay.forEach(valueDisplay=>{
+    let startValue =0;
+    let endValue = parseInt(valueDisplay.getAttribute
+        ("data-val"));
+    console.log(endValue)    
+    let duration =Math.floor(interval/endValue);
+    let counter= setInterval(function(){
+        startValue +=1;
+        valueDisplay.textContent = startValue;
+        
+        if (startValue == endValue){
+            clearInterval(counter)
+
+        }
+    }, duration);
+})
